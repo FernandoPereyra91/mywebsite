ff231f92c4986d03f542e6f8db66f9127c15ceee
Vargas Pablo Victor <vargas.pablo.victor@gmail.com>
2021-06-27 17:42:06 -0300

    jugando con javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-alert("Fernando")
+
+var num1 = prompt("ingrese primer numero: ")
+var num2 = prompt("igrese segundo nombre: ")
+
+resultado = parseInt(num1) / parseInt(num2) 
+
+// te lo escribe en el html
+document.write(resultado)
+
+alert(resultado)
+
